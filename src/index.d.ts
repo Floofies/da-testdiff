2c04498d024e83a444e54a7fbf22d3ba19e9dcfa
Floofies <fantasticdragons@gmail.com>
2023-04-21 15:07:46 -0400

    Fixed missing JSDoc in index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,7 @@
+/**
+ * Returns true if input1 differs in any way from input2. Performs "deep" object/array traversal by default, comparing all reachable values.
+ * @param {any} input1 A value/object to compare against input2.
+ * @param {any} input2 A value/object to compare against input1.
+ * @param {boolean} [deep=true] Set this operand to false to disable traversal and nested comparisons.
+ */
 export default function testDiff(input1:any|object|Array<any>, input2:any|object|Array<any>, deep:boolean):boolean;
